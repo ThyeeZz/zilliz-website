c3e026466c6f0bddefc73626f92e6c39623f661f
ThyeeZz <jialian.ji@zilliz.com>
2023-08-08 17:22:22 +0800

    debug
    Signed-off-by: ThyeeZz <jialian.ji@zilliz.com>

--- apps/zilliz.com.en/src/app/page.tsx
+++ apps/zilliz.com.en/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import '../../../../styles/fonts.css';
 
 export default function Home() {
   return (
@@ -49,7 +50,9 @@ export default function Home() {
           <h2>
             Docs <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p style={{ fontFamily: 'SF Mono' }}>
+            Find in-depth information about Next.js features and API.
+          </p>
         </a>
 
         <a
